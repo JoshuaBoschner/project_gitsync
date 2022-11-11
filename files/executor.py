098c84e69bc37477dbc016176d07c58df2ddd2a5
Joshua Boschner <jboschner.itsb-m2021@fh-salzburg.ac.at>
2022-11-11 16:12:22 +0100

    Updated from python script

--- files/executor.py
+++ files/executor.py
@@ -1,47 +1,47 @@
-from files.log import get_commit_message, print_add_message, print_commit_message, print_custom_error_message, print_info_message
-
-
-def git_push(repo):
-    print_info_message('Trying to push changes to remote repository')
-    try:
-        origin = repo.remote(name='origin')
-        origin.push()
-    except:
-        print_custom_error_message('Failed to push changes to remote repository')
-        raise
-    finally:
-        print_info_message('Successfully push changes to remote repository')
+from files.log import get_commit_message, print_add_message, print_commit_message, print_custom_error_message, print_info_message, print_pull_message
 
 
 def git_add(repo):
-    print_info_message('Trying to add changes from working directory to staging area')
+    print_info_message('Trying to add changes from working directory to staging area ...')
     try:
         repo.git.add(all=True)
     except:
-        print_custom_error_message('Failed to add changes from working directory to staging area')
+        print_custom_error_message('Failed to add changes from working directory to staging area.')
         raise
     finally:
-        print_add_message('Successfully added changes from working directory to staging area')
+        print_add_message('Successfully added changes from working directory to staging area!')
 
 
 def git_commit(repo):
-    print_info_message('Trying to commit staged changes to local repository')
+    print_info_message('Trying to commit staged changes to local repository ...')
     try:
         commit_message = get_commit_message()
         repo.index.commit(commit_message)
     except:
-        print_custom_error_message('Failed to commit staged changes to local repository')
+        print_custom_error_message('Failed to commit staged changes to local repository.')
         raise
     finally:
-        print_commit_message('Successfully committed staged changes to local repository')
+        print_commit_message('Successfully committed staged changes to local repository!')
 
 
 def git_pull(repo):
-    print_info_message('Trying to pull changes from remote repository')
+    print_info_message('Trying to pull changes from remote repository ...')
     try:
         repo.git.pull()
     except:
-        print_custom_error_message('Failed to pull changes from remote repository')
+        print_custom_error_message('Failed to pull changes from remote repository.')
         raise
     finally:
-        print_info_message('Successfully pulled changes from remote repository')
+        print_pull_message('Successfully pulled changes from remote repository!')
+
+
+def git_push(repo):
+    print_info_message('Trying to push changes to remote repository ...')
+    try:
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print_custom_error_message('Failed to push changes to remote repository.')
+        raise
+    finally:
+        print_info_message('Successfully push changes to remote repository!')
